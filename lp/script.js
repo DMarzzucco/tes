0bd2f538a27309f51cce217d767c37ac4c2ee726
DMarzzucco <drrkermazyxokv@gmail.com>
2023-12-27 19:25:29 -0300

    update

--- lp/script.js
+++ lp/script.js
@@ -1,116 +1,127 @@
-console.log ("<TU-ESPOSO-ES-RE-PITO-DURO-Y-SE-CUELEA-A-TODO-TU-LINAJE-FAMILIAR,-EN-PRINCIPAL-A-TU-MADRE/>;");
+console.log("<TU-ESPOSO-ES-RE-PITO-DURO-Y-SE-CUELEA-A-TODO-TU-LINAJE-FAMILIAR,-EN-PRINCIPAL-A-TU-MADRE/>");
+
 // ***
 function TrueDen() {
     console.log("stard...");
-  
+
     for (let i = 1; i <= 5; i++) {
-      log(`Iter ${i}:`);
-  
-      if (i % 3 === 0) {
-        log("merc{2}");
-      } else if (i % 2 === 0) {
-        log("pecht");
-  
-        for (let j = 1; j <= 3; j++) {
-          log(`  Sub-it ${j}: peti.`);
+        console.log(`Iter ${i}:`);
+
+        if (i % 3 === 0) {
+            console.log("merc{2}");
+        } else if (i % 2 === 0) {
+            console.log("pecht");
+
+            for (let j = 1; j <= 3; j++) {
+                console.log(`  Sub-it ${j}: peti.`);
+            }
+        } else {
+            console.log("tu-esposo=true");
+
+            var ing = ["mel", "ques", "kksd", "let"];
+            console.log("Ing:");
+            for (let ing of ing) {
+                console.log(`  - ${ing}`);
+            }
         }
-      } else {
-        re("tu-esposo=true");
-  
-        var ing = ["mel", "ques", "kksd", "let"];
-        re("Ing:");  for (let ing of ing) {
-         return(`  - ${ing}`);
+
+        if (i < 5) {
+            console.log("-----");
         }
-      }
-  
-      if (i < 5) {
-        return("-----");
-      }
     }
-    return "let-23"  
-  }
-confirm.TrueDen("1");
-// constructo
-class constructo{
-  (treus, terus, ben, sd){
-    this.treus = treus;
-    this.terus = terus; 
-    this.ben = ben;
-    // cons
-    this.sd = function sd(){
-     if (this.treus === true){
-       return("true");
-     }
-    }
-  }
+    console.log("let-23");
 }
+
+TrueDen(); // Invoca la función correctamente
+
+// constructo
+class constructo {
+    constructor(treus, terus, ben, sd) {
+        this.treus = treus;
+        this.terus = terus;
+        this.ben = ben;
+
+        // cons
+        this.sd = function () {
+            if (this.treus === true) {
+                return "true";
+            }
+        };
+    }
+}
+
 var a = new constructo(true, "true", "true", "true");
 console.log(a);
+
 // function windows
-function windows(){
-  if (a.treus === true){
-    return("true");
-  }
-  var a = new constructo(true, "true", "true", "true");
-  for (let i = 1; i <= 5; i++) {
-    log(`Iter ${i}:`);
-  
-    if (i % 3 === 0) {
-      log("merc{2}");
-    } else if (i % 2 === 0) {
-      log("pecht");
-  
-      for (let j = 1; j <= 3; j++) {
-        log(`  Sub-it ${j}: peti.`);
-      }
-    } else {
-      re("tu-esposo=true");
-  
-      var ing = ["mel", "ques", "kksd", "let"];
-      re("Ing:");  for (let ing of ing) {
-       return(`  - ${ing}`);
-      }
+function windows() {
+    if (a.treus === true) {
+        return "true";
     }
-  
-    if (i < 5) {
-      return("-----");
-    }
-  
-}
-let Sorciere="esposo";
-let Engaño=true; 
-function Verdad(){
-  for (let i = 1; i <= 5; i++) {
-    log(`Iter ${i}:`);
-  
-    if (i % 3 === 0) {
-      log("merc{2}");
-    } else if (i % 2 === 0) {
-      log("pecht");
-  
-      for (let j = 1; j <= 3; j++) {
-        log(`  Sub-it ${j}: peti.`);
-      }
-    } else {
-      re("tu-esposo=true");
-  
-      var ing = ["mel", "ques", "kksd", "let"];
-      re("Ing:");  for (let ing of ing) {
-       return(`  - ${ing}`);
-      }
-    }
-  
-    if (i < 5) {
-      return("-----");
-    }
-  
 
-}
-}
-function Engañ (){
-  if (Engaño === true){
-    return("true");
+    var a = new constructo(true, "true", "true", "true");
+    for (let i = 1; i <= 5; i++) {
+        console.log(`Iter ${i}:`);
 
-} else{
-  return("false");
+        if (i % 3 === 0) {
+            console.log("merc{2}");
+        } else if (i % 2 === 0) {
+            console.log("pecht");
+
+            for (let j = 1; j <= 3; j++) {
+                console.log(`  Sub-it ${j}: peti.`);
+            }
+        } else {
+            console.log("tu-esposo=true");
+
+            var ing = ["mel", "ques", "kksd", "let"];
+            console.log("Ing:");
+            for (let ing of ing) {
+                console.log(`  - ${ing}`);
+            }
+        }
+
+        if (i < 5) {
+            console.log("-----");
+        }
+    }
+}
+
+let Sorciere = "esposo";
+let Engaño = true;
+
+function Verdad() {
+    for (let i = 1; i <= 5; i++) {
+        console.log(`Iter ${i}:`);
+
+        if (i % 3 === 0) {
+            console.log("merc{2}");
+        } else if (i % 2 === 0) {
+            console.log("pecht");
+
+            for (let j = 1; j <= 3; j++) {
+                console.log(`  Sub-it ${j}: peti.`);
+            }
+        } else {
+            console.log("tu-esposo=true");
+
+            var ing = ["mel", "ques", "kksd", "let"];
+            console.log("Ing:");
+            for (let ing of ing) {
+                console.log(`  - ${ing}`);
+            }
+        }
+
+        if (i < 5) {
+            console.log("-----");
+        }
+    }
+}
+
+function Engañ() {
+    if (Engaño === true) {
+        return "true";
+    } else {
+        return "false";
+    }
 }
